49378b81b211353424ad0184f710b9b95a1ed566
David Retegan <david.retegan@icloud.com>
2022-01-01 19:09:09 +0200

    iOS 13 optimizing

--- Roadout/Roadout/Interface/Settings/Extras/AddReminderViewController.swift
+++ Roadout/Roadout/Interface/Settings/Extras/AddReminderViewController.swift
@@ -12,10 +12,34 @@ class AddReminderViewController: UIViewController {
     let UserDefaultsSuite = UserDefaults.init(suiteName: "group.ro.roadout.Roadout")!
     
     let setTitle = NSAttributedString(string: "Set", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .medium)])
+    
+    var oldSelectedDate = Date().addingTimeInterval(900)
 
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var blurButton: UIButton!
     @IBOutlet weak var cancelBtn: UIButton!
+    
+    @IBOutlet weak var pickBtn: UIButton!
+    @IBAction func pickTapped(_ sender: Any) {
+        let alertController = UIAlertController(title: "\n\n\n\n\n\n\n\n", message: nil, preferredStyle: UIAlertController.Style.alert)
+        let alertDatePicker: UIDatePicker = UIDatePicker()
+        alertDatePicker.frame = CGRect(x: 0, y: 15, width: alertController.view.frame.width, height: 200)
+        alertDatePicker.minimumDate = Date().addingTimeInterval(900)
+        if #available(iOS 14.0, *) {
+            alertDatePicker.preferredDatePickerStyle = .wheels
+        }
+        alertDatePicker.minuteInterval = 15
+        alertController.view.addSubview(alertDatePicker)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.oldSelectedDate = alertDatePicker.date
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        alertController.view.tintColor = UIColor(named: "Icons")
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     
     @IBOutlet weak var setBtn: UIButton!
     @IBAction func setTapped(_ sender: Any) {
@@ -27,11 +51,19 @@ class AddReminderViewController: UIViewController {
         }
         if ok == true {
             if labelField.text != "" {
-                let reminder = Reminder(label: labelField.text!, date: datePicker.date, identifier: "ro.roadout.reminder.\(labelField.text!.replacingOccurrences(of: " ", with: ""))")
-                if UserPrefsUtils.sharedInstance.reminderNotificationsEnabled() {
-                    NotificationHelper.sharedInstance.scheduleReminder(reminder: reminder)
+                if #available(iOS 14.0, *) {
+                    let reminder = Reminder(label: labelField.text!, date: datePicker.date, identifier: "ro.roadout.reminder.\(labelField.text!.replacingOccurrences(of: " ", with: ""))")
+                    if UserPrefsUtils.sharedInstance.reminderNotificationsEnabled() {
+                        NotificationHelper.sharedInstance.scheduleReminder(reminder: reminder)
+                    }
+                    saveReminder(reminder: reminder)
+                } else {
+                    let reminder = Reminder(label: labelField.text!, date: oldSelectedDate, identifier: "ro.roadout.reminder.\(labelField.text!.replacingOccurrences(of: " ", with: ""))")
+                    if UserPrefsUtils.sharedInstance.reminderNotificationsEnabled() {
+                        NotificationHelper.sharedInstance.scheduleReminder(reminder: reminder)
+                    }
+                    saveReminder(reminder: reminder)
                 }
-                saveReminder(reminder: reminder)
                 NotificationCenter.default.post(name: .refreshReminderID, object: nil)
                 UIView.animate(withDuration: 0.1) {
                     self.blurButton.alpha = 0
@@ -94,11 +126,24 @@ class AddReminderViewController: UIViewController {
         setBtn.layer.cornerRadius = 13.0
         setBtn.setAttributedTitle(setTitle, for: .normal)
         
+        datePicker.minimumDate = Date().addingTimeInterval(900)
+        
         labelField.layer.cornerRadius = 12.0
         labelField.attributedPlaceholder = NSAttributedString(
             string: "Notification Label",
             attributes: [NSAttributedString.Key.foregroundColor: UIColor(named: "Greyish")!, NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .medium)]
         )
+        if #available(iOS 14.0, *) {
+            self.pickBtn.isEnabled = false
+            self.pickBtn.isHidden = true
+            self.datePicker.isEnabled = true
+            self.datePicker.isHidden = false
+        } else {
+            self.pickBtn.isEnabled = true
+            self.pickBtn.isHidden = false
+            self.datePicker.isEnabled = false
+            self.datePicker.isHidden = true
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
